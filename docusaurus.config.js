7cdd2dd8d4b8be0081aefe220a3bad878e209cbe
Luis Moreira <luismoreirapple@gmail.com>
2025-11-24 16:47:30 +0000

    Alterei

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -20,9 +20,7 @@ const config = {
   },
 
   // Set the production url of your site here
-  url: "https://github.com",
-  // Set the /<baseUrl>/ pathname under which your site is served
-  // For GitHub pages deployment, it is often '/<projectName>/'
+  url: "https://luismore1ra.github.io",
   baseUrl: "/TI2025-dokku/",
 
   // GitHub pages deployment config.
